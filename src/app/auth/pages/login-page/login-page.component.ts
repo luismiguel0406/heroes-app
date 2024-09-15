6fd7fbb48200e8000db0dc8e23d0ceb76f1a1b18
Luis del orbe <luismigueldelorbe@hotmail.com>
2024-09-15 13:00:12 -0400

    Trabajando seccion 13

---
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,8 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-login-page',
+    templateUrl: 'login-page.component.html'
+})
+export class LoginPageComponent { }
---
+++ src/app/shared/pages/error404-page/error404-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'error404-page',
+    templateUrl: 'error404-page.component.html'
+})
+
+export class Error404Component implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
